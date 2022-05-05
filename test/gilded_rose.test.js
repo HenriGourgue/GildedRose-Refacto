2dbcd3bc2ce1deea5455e6f35d6cbe6dcad05173
Henri Gourgue <gourguehenri@gmail.com>
2022-05-05 17:34:15 +0200

    Tests for conjured items (not coded yet)

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -115,3 +115,33 @@ describe("Backstage passes to a TAFKAL80ETC", () => {
     expect(results[0].quality).toBe(12);
   });
 });
+
+//Tests pour les items conjurés (pas encore géré dans le code)
+describe("Cursed Dark Wizard's Hand", () => {
+
+  const name = "Cursed Dark Wizard's Hand";
+
+  it("Test vérification que la qualité descend deux fois plus vite si sellIn est à 0", function () {
+
+    const shop = new Shop([new Item(name, 0, 5)]);
+    const results = shop.updateQuality();
+
+    expect(results[0].quality).toBe(1);
+  });
+
+  it("Test vérification que la qualité descend deux fois plus vite qu'un item standard", function () {
+
+    const shop = new Shop([new Item(name, 10, 12)])
+    const results = shop.updateQuality();
+
+    expect(results[0].quality).toBe(10);
+  });
+
+  it("Test vérification que la qualité ne descend pas sous 0", function () {
+
+    const shop = new Shop([new Item(name, 0, 0)]);
+    const results = shop.updateQuality();
+
+    expect(results[0].quality).toBe(0);
+  })
+});
