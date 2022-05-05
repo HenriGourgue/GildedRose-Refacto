160554c66861345b04da1e461642aff4a3ec6ef3
Henri Gourgue <gourguehenri@gmail.com>
2022-05-05 17:26:12 +0200

    Backstage

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -72,9 +72,46 @@ describe("Sulfuras", () => {
 
   it("Test vérification que la qualité est toujours à 80 (item légendaire)", function() {
 
-    const shop = new Shop([new Item(name, 10, 80)]);
+    const shop = new Shop([new Item(name, 5, 80)]);
     const results = shop.updateQuality();
 
     expect(results[0].quality).toBe(80);
+  });
+});
+
+describe("Backstage passes to a TAFKAL80ETC", () => {
+
+  const name = "Backstage passes to a TAFKAL80ETC concert";
+
+  it("Test vérification si moins de 5 jours de sellIn, la qualité augmente de 3", function() {
+
+    const shop = new Shop([new Item(name, 2, 6)]);
+    const results = shop.updateQuality();
+
+    expect(results[0].quality).toBe(9);
+  });
+
+  it("Test si le sellIn est à 0 (concert terminé), la qualité tombe à 0", function() {
+
+    const shop = new Shop([new Item(name, 0, 5)]);
+    const results = shop.updateQuality();
+
+    expect(results[0].quality).toBe(0);
+  });
+
+  it("Test vérification si moins de 10 jours, la qualité augmente de 2", function() {
+
+    const shop = new Shop([new Item(name, 8, 11)]);
+    const results = shop.updateQuality();
+
+    expect(results[0].quality).toBe(13);
+  });
+
+  it("Test vérification si plus de 10 jours de sellIn, la qualité augmente de 1", function() {
+
+    const shop = new Shop([new Item(name, 11, 11)]);
+    const results = shop.updateQuality();
+
+    expect(results[0].quality).toBe(12);
   });
 });
