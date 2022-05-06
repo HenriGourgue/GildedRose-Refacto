3725506452b0ed2ae5d27a7b498787bda34d446e
Henri Gourgue <gourguehenri@gmail.com>
2022-05-06 17:36:53 +0200

    Standard items updateQuality method (+ tests passed)

--- src/class/Standard.js
+++ src/class/Standard.js
@@ -7,6 +7,25 @@ class Standard extends Item {
         super(name, sellIn, quality);
     }
 
+    updateQuality(){
+
+        this.sellIn--;
+
+        if(this.sellIn < 0){
+            if(this.quality - 2 < 0){
+                this.quality = 0;
+            } else {
+                this.quality = this.quality - 2;
+            }
+        } else {
+            if(this.quality - 1 < 0){
+                this.quality = 0;
+            } else {
+                this.quality --;
+            }
+        }
+    }
+
 }
 
 module.exports = {
